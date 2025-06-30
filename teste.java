fe8090a863d22521978a6ebe681a983f73a102ca
igor <igor34guilherme@gmail.com>
2025-06-30 14:13:27 -0300

    Projeto Demo Spring

---
+++ teste.java
@@ -0,0 +1,14 @@
+/* 
+Metodos devem SEMPRE pertencer a uma classe
+Metodos soltos não funcionam, tudo em java está dentro de uma classe
+*/
+class Main {
+    public static void main(String[] args) {
+        /* Declarações de tipos primitivos:
+            <tipo> <nome> = <valor> (é opcional);
+        São armazenadas na stack da memória RAM */
+        int idade = 25;
+        double altura = 1.72;
+        char sexo = 'M';
+    }
+}
